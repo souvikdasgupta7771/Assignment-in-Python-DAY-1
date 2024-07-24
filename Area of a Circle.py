d8d65ffcbb4f90f1ad3eb9a5d266a4a1bdede40c
souvikdasgupta7771 <souviktech77@gmail.com>
2024-07-24 10:32:26 +0530

    Create Area of a Circle.py

---
+++ Area of a Circle.py
@@ -0,0 +1,8 @@
+#AREA OF A CIRCLE ASSIGNMENT 1
+print("The program will calculate the AREA of a CIRCLE")
+
+radius=float(input("Please Enter the Radius of a circle:"))
+pi=3.14
+area=pi*(radius*radius)
+print("The area of your circle is",area)
+print("The area of the circle is %0.2f"%area)
