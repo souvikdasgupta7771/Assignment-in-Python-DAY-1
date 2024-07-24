7e6b7a73887ae32e2d07febb1936ad08f1e0fec1
souvikdasgupta7771 <souviktech77@gmail.com>
2024-07-24 10:35:58 +0530

    Create User Information.py

---
+++ User Information.py
@@ -0,0 +1,10 @@
+"""Assignment 6: User Information Write a program that takes
+the user's name, age, and favorite color as inputs and then print them in a formatted message"""
+
+print("This is a string formatting application - ")
+
+name=input("Please enter your full name : ")
+age=int(input("Please enter your age : "))
+favorite_color=input("Please enter your favourite color : ")
+
+print("Hi,",name,".I can see your age is ",age," and your favourite color is ",favorite_color)
