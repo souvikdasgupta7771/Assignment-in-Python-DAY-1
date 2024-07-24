8e50d999a2a3ca23028300e8382bb175c9f265a6
souvikdasgupta7771 <souviktech77@gmail.com>
2024-07-24 10:37:37 +0530

    Create Character Count .py

---
+++ Character Count .py
@@ -0,0 +1,8 @@
+#Assignment 9: Character Count Write a program that takes a
+#string as input and counts the number of occurrences of a
+#specific character entered by the user.
+
+print("Character Count program - ")
+sentence=input("Please enter a sentence : ")
+specific_character=input("Please enter a specific_character which you want to find: ")
+print("Number of occurrences",sentence.count(specific_character))
