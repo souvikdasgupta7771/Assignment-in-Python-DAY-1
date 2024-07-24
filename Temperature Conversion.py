9fc5f39d426b14b4f83bbc1aa4eb69158c129e72
souvikdasgupta7771 <souviktech77@gmail.com>
2024-07-24 10:33:54 +0530

    Create Temperature Conversion.py

---
+++ Temperature Conversion.py
@@ -0,0 +1,8 @@
+#Assignment 2 FAHRENHEIT TO CELSIUS
+
+print("The Program will covert the temperature from FAHRENHEIT to CELSIUS.")
+
+Fahrenheit=float(input("Please Enter the temperature in Fahrenheit: "))
+Celsius=(Fahrenheit-32) *5/9
+print("Temperature in Celsius:",Celsius)
+print("Temperature in Celsius will be:%0.3f"%Celsius)
