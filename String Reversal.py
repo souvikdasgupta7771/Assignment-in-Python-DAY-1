809921acb2d53584cf42fb9b3d41b6b229117105
souvikdasgupta7771 <souviktech77@gmail.com>
2024-07-24 10:35:30 +0530

    Create String Reversal.py

---
+++ String Reversal.py
@@ -0,0 +1,7 @@
+#Assignment 5 String Reversal
+
+print("This is a String Reversal Program")
+
+given_string=input("Please Enter Your Name: ")
+reversed_string=given_string[::-1]
+print("String in Reverse: ",reversed_string)
