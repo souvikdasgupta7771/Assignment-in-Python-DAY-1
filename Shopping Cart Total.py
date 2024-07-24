6c748464f894ba173dccee6a6bcc82a8e05633b8
souvikdasgupta7771 <souviktech77@gmail.com>
2024-07-24 10:37:02 +0530

    Create Shopping Cart Total.py

---
+++ Shopping Cart Total.py
@@ -0,0 +1,7 @@
+#Assignment 8: Shopping Cart Total Write a program that
+print("Shopping Cart Total - ")
+T_shirt_price=float(input("Please enter the price of T-shirt: "))
+T_shirt_quantity=int(input("How many T-shirts you bought: "))
+Saree_price=float(input("Please enter the price of Saree: "))
+Saree_quantity=float(input("How many Sarees you bought : "))
+print("Your subtotal is ",(T_shirt_price*T_shirt_quantity)+(Saree_price*Saree_quantity))
